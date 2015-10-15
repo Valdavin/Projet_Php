53e585db013f1979b47ea99673b05a5aa78a8616
valdavin <dj_valentin@hotmail.fr>
2015-10-15 11:39:23 +0200

    Class RSS et Nouvelle

--- model/RSS.class.php
+++ model/RSS.class.php
@@ -1,3 +1,4 @@
+<?php
       class RSS {
         private $titre; // Titre du flux
         private $url;   // Chemin URL pour télécharger un nouvel état du flux
@@ -13,5 +14,37 @@
         function titre() {
           return $this->titre;
         }
-        ...
+
+        function url() {
+          return $this->url;
+        }
+
+        function date() {
+          return $this->date;
+        }
+
+        function nouvelles() {
+          return $this->nouvelles;
+        }
+
+        // Autres methodes
+
+        function update() {
+		    // Cree un objet pour accueillir le contenu du RSS : un document XML
+		    $doc = new DOMDocument;
+
+		    //Telecharge le fichier XML dans $rss
+		    $doc->load($this->url);
+
+		    // Recupère la liste (DOMNodeList) de tous les elements de l'arbre 'title'
+		    $nodeList = $doc->getElementsByTagName('title');
+
+		    // Met à jour le titre dans l'objet
+		    $this->titre = $nodeList->item(0)->textContent;
+
+		    
+        }
+        
+        
       }
+?>
