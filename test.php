49b4965898aa4ca3de966a67ecd5543a34037db7
GLIERE Swann <glieres@hostname.iut2.upmf-grenoble.fr>
2015-10-15 11:40:16 +0200

    test v final

--- test.php
+++ test.php
@@ -1,13 +1,27 @@
 <?php
 
-	$doc = new DOMDocument;
-	$doc->load('http://www.lemonde.fr/m-actu/rss_full.xml');
-	$titreNodeList = $doc->getElementsByTagName('title');
-	  $titrePrinc = $titreNodeList->item(0)->textContent;
-      printf("<h1>%s</h1> </br>",$titrePrinc);
-	$docNodeList =  $doc->getElementsByTagName('item');
-	foreach ($docNodeList as $nouvelle) {    	
-		$title = $nouvelle->getElementsByTagName('title')->item(0)->textContent;
-   	 printf("%s </br>",$title);
+$doc = new DOMDocument;
+$doc->load('http://www.lemonde.fr/m-actu/rss_full.xml');
+$titreNodeList = $doc->getElementsByTagName('title');
+$titrePrinc = $titreNodeList->item(0)->textContent;
+printf("<h1>%s</h1> </br>",$titrePrinc);
+$docNodeList =  $doc->getElementsByTagName('item');
+foreach ($docNodeList as $nouvelle) {    	
+	$title = $nouvelle->getElementsByTagName('title')->item(0)->textContent;
+	printf("%s </br>",$title);
+	$nodeList = $nouvelle->getElementsByTagName('enclosure');
+	if($nodeList->length != 0) {
+		$node = $nodeList->item(0);
+
+		$node = $node->attributes->getNamedItem('url');
+		if ($node != NULL) {
+			$url = $node->nodeValue;
+			$nomLocalImage=rand();
+			$image = 'images/'.$nomLocalImage.'.jpg';
+			file_put_contents($image, file_get_contents($url));
+		} else {
+			$this->image = "";
+		}
 	}
+}
 ?>
