cf77833a92fa6c0ee8b4d339e99145b0d13fbf79
valdavin <dj_valentin@hotmail.fr>
2015-10-15 10:58:52 +0200

    Et test 3

--- model/RSS.class.php
+++ model/RSS.class.php
@@ -1,1 +1,17 @@
- 
+      class RSS {
+        private $titre; // Titre du flux
+        private $url;   // Chemin URL pour télécharger un nouvel état du flux
+        private $date;  // Date du dernier téléchargement du flux
+        private $nouvelles; // Liste des nouvelles du flux
+
+        // Contructeur
+        function __construct($url) {
+          $this->url = $url;
+        }
+
+        // Fonctions getter
+        function titre() {
+          return $this->titre;
+        }
+        ...
+      }
