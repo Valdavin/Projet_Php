d5ec6c4f6aa6487aa3b9fd4852c08d125f1db0a2
valdavin <dj_valentin@hotmail.fr>
2015-11-04 08:54:46 +0100

    début controleurs

---
+++ controler/afficher_flux.ctrl.php
@@ -0,0 +1,51 @@
+<?php
+
+
+//////////////////////////////////////////////////////////////////////////////
+// PARTIE RECUPERATION DES DONNEES
+//////////////////////////////////////////////////////////////////////////////
+if (isset($_GET['envoyer'])) {
+  $choix = $_GET['envoyer'];
+} else {
+  // C'est une erreur : on doit toujours avoir un nom
+  $error = "afficher_flux.ctrl : le string envoyer s'est perdu";
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// PARTIE USAGE DU MODELE
+//////////////////////////////////////////////////////////////////////////////
+
+// Calculs : rien à faire
+
+
+//////////////////////////////////////////////////////////////////////////////
+// PARTIE SELECTION DE LA VUE
+//////////////////////////////////////////////////////////////////////////////
+
+// Choix de la vue en fonction de l'état des variables
+if (!isset($error)) {
+	switch $envoyer {
+		case "vue1" :
+			include("../view/uniquementImage.view.php");
+			break;
+		case "vue2" :
+			include("../view/imageTitreDesc.view.php");
+			break;
+		case "vue3" :
+			include("../view/affichage_flux.view.php");
+			break;
+		default : 
+			$error  = "afficher_flux.ctrl : Mauvais choix de vue"
+			$data['error'] = $error;
+	  		// On charge la vue
+	  		include("../view/error.view.php");
+	}
+	
+} else {
+	$data['error'] = $error;
+}
+
+
+
+
+?>
--- view/uniquementImage.view.php
+++ view/uniquementImage.view.php
@@ -7,7 +7,16 @@
 		<?php
 		 foreach ($nouvelles as $img) {
            ?> 	<!--  <img src=<?php echo $img->image() ?> alt="some_text" style="width:300px; height:150px;"> --> 
-           		<input type="image" name="photo" src=<?php echo $img->image()?> style="width:300px; height:150px;" > </input>
+           		
+              <input  type="image" 
+                      name="photo" 
+                      src=<?php echo $img->image()?>
+                      title=<?php echo "\" ".
+                                  $img->description().
+                                  "\" ";
+                                  ?>
+                      style="width:300px; height:150px;" > 
+              </input>
       	 <?php
       	}
       	?>
